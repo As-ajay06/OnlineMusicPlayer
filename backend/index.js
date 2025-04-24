3fae1cd8b3dd803adf33d27ebe94a32c15b6c51c
As-ajay06 <as2673447@gmail.com>
2025-04-24 16:58:30 +0530

    fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const userRouter = require('./src/routes/userRouter')
-const adminRouter = require('./src/routes/adminRouter')
-const spotifyRouter = require('./routes/spotifyRouter')
+const artistRouter = require('./src/routes/artistRouter')
 
 const port = 3000;
 
 
 app.use(express.json())
-
-app.use("api/v1/user", userRouter)
-app.use("api/v1/spotify", spotifyRouter)
+app.use("/api/v1/user", userRouter)
+app.use("/api/v1/spotify", artistRouter)
   
 
 app.listen(port, console.log("listening on port " + port))
